4a495eed73077fbcfb36bd9182e1606a4764304a
Michael Robinson <mike@pagesofinterest.net>
2012-12-04 01:04:51 +1300

    Moved relevant code into ACEWebView

--- ACEView/Source/ACEView.m
+++ ACEView/Source/ACEView.m
@@ -6,25 +6,19 @@
 //  Copyright (c) 2012 Code of Interest. All rights reserved.
 //
 
-#import <WebKit/WebKit.h>
-
 #import <ACEView/ACEView.h>
+#import <ACEView/ACEWebView.h>
 #import <ACEView/ACEModeNames.h>
 #import <ACEView/ACEThemeNames.h>
 
 #define ACE_JAVASCRIPT_DIRECTORY @"___ACE_VIEW_JAVASCRIPT_DIRECTORY___"
 
-@interface ACEView() {
-    WebView *_webView;
-    CGColorRef _borderColor;
-}
+@interface ACEView() { }
 
 - (CGColorRef) borderColor;
-- (WebView *) webView;
+- (ACEWebView *) webView;
 - (void) initialiseWebView;
 - (void) embedWebView;
-- (void) executeScriptsWhenLoaded:(NSArray *)scripts;
-- (void) executeScriptWhenLoaded:(NSString *)script;
 
 @end
 
@@ -56,9 +50,9 @@
     }
     return _borderColor;
 }
-- (WebView *)webView {
+- (ACEWebView *) webView {
     if (_webView == nil) {
-        _webView = [[WebView alloc] initWithFrame:self.frame];
+        _webView = [[ACEWebView alloc] initWithFrame:self.frame];
     }
     return _webView;
 }
@@ -89,22 +83,6 @@
     NSRect bounds = NSMakeRect(self.bounds.origin.x - 1, self.bounds.origin.y - 1, self.bounds.size.width - 1, self.bounds.size.height - 1);
     [webView setFrame:bounds];
 }
-- (void) executeScriptsWhenLoaded:(NSArray *)scripts {
-    if ([[self webView] isLoading]) {
-        [self performSelector:@selector(executeScriptsWhenLoaded:) withObject:scripts afterDelay:0.2];
-        return;
-    }
-    [scripts enumerateObjectsUsingBlock:^(id script, NSUInteger index, BOOL *stop) {
-        [[self webView] stringByEvaluatingJavaScriptFromString:script];
-    }];
-}
-- (void) executeScriptWhenLoaded:(NSString *)script {
-    if ([[self webView] isLoading]) {
-        [self performSelector:@selector(executeScriptWhenLoaded:) withObject:script afterDelay:0.2];
-        return;
-    }
-    [[self webView] stringByEvaluatingJavaScriptFromString:script];
-}
 
 #pragma mark - Public
 - (NSString *) content {
@@ -117,14 +95,18 @@
     
     content = [jsonString substringWithRange:NSMakeRange(2, jsonString.length - 4)];
     
-    [self executeScriptsWhenLoaded:@[[NSString stringWithFormat:@"editor.setValue(\"%@\");", content], @"editor.clearSelection();"]];
+    [[self webView] executeScriptsWhenLoaded:@[
+        [NSString stringWithFormat:@"editor.setValue(\"%@\");", content],
+        @"editor.clearSelection();",
+        @"editor.moveCursorTo(0, 0);"
+    ]];
 }
 
 - (void) setMode:(ACEMode)mode {
-    [self executeScriptWhenLoaded:[NSString stringWithFormat:@"editor.getSession().setMode(\"ace/mode/%@\");", [ACEModeNames nameForMode:mode]]];
+    [[self webView] executeScriptWhenLoaded:[NSString stringWithFormat:@"editor.getSession().setMode(\"ace/mode/%@\");", [ACEModeNames nameForMode:mode]]];
 }
 - (void) setTheme:(ACETheme)theme {
-    [self executeScriptWhenLoaded:[NSString stringWithFormat:@"editor.setTheme(\"ace/theme/%@\");", [ACEThemeNames nameForTheme:theme]]];
+    [[self webView] executeScriptWhenLoaded:[NSString stringWithFormat:@"editor.setTheme(\"ace/theme/%@\");", [ACEThemeNames nameForTheme:theme]]];
 }
 
 @end
