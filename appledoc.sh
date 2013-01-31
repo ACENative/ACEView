5b7626eec795e1ea8fde344d5833183c044605c3
Michael Robinson <mike@pagesofinterest.net>
2013-02-01 00:04:26 +1300

    Added appledoc script

---
+++ appledoc.sh
@@ -0,0 +1,19 @@
+/usr/local/bin/appledoc \
+--project-name "ACEView" \
+--project-company "Code of Interest" \
+--company-id "com.codeofinterest" \
+--docset-atom-filename "ACEView.atom" \
+--docset-feed-url "http://faceleg.github.com/ACEView/%DOCSETATOMFILENAME" \
+--docset-package-url "http://faceleg.github.com/ACEView/%DOCSETPACKAGEFILENAME" \
+--docset-fallback-url "http://faceleg.github.com/ACEView/" \
+--output "~/help" \
+--publish-docset \
+--logformat xcode \
+--keep-undocumented-objects \
+--keep-undocumented-members \
+--keep-intermediate-files \
+--no-repeat-first-par \
+--no-warn-invalid-crossref \
+--ignore "*.m" \
+--index-desc "README.md" \
+.
